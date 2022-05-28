740a782c07e17b9ccb2bc3f302cacce4db0ef083
JosephJoshua <jj.anggita@gmail.com>
2022-05-29 05:41:42 +0700

    Complete the /ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,25 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with Pong!'),
+        .setDescription('Pings the server.'),
     async execute(interaction) {
-        await interaction.reply('Pong!');
+        const reply = await interaction.reply({ content: 'Pong!', fetchReply: true });
+        const latency = reply.createdTimestamp - interaction.createdTimestamp;
+        const apiPing = interaction.client.ws.ping;
+        const bot = interaction.client.user;
+
+        const embed = new MessageEmbed()
+            .setTitle('Pong!')
+            .setColor(interaction.client.colors.primary)
+            .setThumbnail(bot.avatarURL())
+            .addField('Latency', `${latency}ms`, true)
+            .addField('API Ping', `${apiPing}ms`, true)
+            .setFooter({ text: bot.username, iconURL: bot.avatarURL() })
+            .setTimestamp();
+
+        await interaction.editReply({ content: '\u200b', embeds: [embed] });
     },
 };
