ca73f984c1fe6caf40f41a7182307f66964cab9a
Sherrin0825 <24986026+Sherrin0825@users.noreply.github.com>
2022-05-29 15:17:57 +0000

    Modify /user command to display roles in a column

--- commands/user.js
+++ commands/user.js
@@ -1,32 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-const splitRolesToColumns = roles => {
-    const roleCols = new Array('', '', '');
-    const maxRowsPerCol = 30;
-
-    let roleIndex = 0;
-
-    for (const val of roles.values()) {
-        const roleName = val.name;
-        const colIndex = Math.floor(roleIndex / maxRowsPerCol);
-
-        if (roleName === '@everyone') continue;
-
-        if (roleIndex >= maxRowsPerCol * roleCols.length - 1) {
-            const elipsis = '........';
-            const lastCol = roleCols[roleCols.length - 1];
-
-            roleCols[roleCols.length - 1] = lastCol + elipsis;
-            break;
-        }
-
-        roleCols[colIndex] += roleName + '\n';
-        roleIndex++;
-    }
-
-    return roleCols;
-};
-
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('user')
@@ -43,26 +16,34 @@ module.exports = {
         const boostingSince = member.premiumSince ? client.bot.date.formatDate(member.premiumSince) : 'Not Boosting';
         const isInVoice = member.voice.channel ? 'Yes' : 'No';
 
+        const EMBED_FIELD_LIMIT = 1024;
+        const ROLE_LENGTH = 22;
+        const ellipses = '...';
+        const separator = ' ';
+
+        const maxRoleCount = Math.floor((EMBED_FIELD_LIMIT - ellipses.length) / (ROLE_LENGTH + separator.length));
+
+        const roles = member.roles.cache;
+        let roleNames = roles
+            .filter(r => r.position) // exclude @everyone
+            .map(r => r.toString());
+
+        const roleCount = roleNames.length;
+        if (roleCount > maxRoleCount) {
+            roleNames = roleNames.slice(0, maxRoleCount);
+            roleNames.push(ellipses);
+        }
+
         const embed = new client.bot.embeds.DefaultEmbed(client)
             .setTitle(`User information - ${member.displayName}`)
             .setThumbnail(member.displayAvatarURL())
-            .addField('Username', member.user.username, true)
+            .addField('Username', member.user.tag, true)
             .addField('ID', member.id, true)
             .addField('Joined Server', client.bot.date.formatDate(member.joinedAt), true)
             .addField('Joined Discord', client.bot.date.formatDate(member.user.createdAt), true)
             .addField('Boosting Since', boostingSince, true)
-            .addField('In Voice', isInVoice, true);
-
-        const roles = member.roles.cache;
-        const roleCols = splitRolesToColumns(roles);
-
-        for (let i = 0; i < roleCols.length; i++) {
-            if (i === 0) {
-                embed.addField(`Roles (${roles.size - 1})`, roleCols[i], true);
-            } else if (roleCols[i] !== '') {
-                embed.addField('\u200b', roleCols[i], true);
-            }
-        }
+            .addField('In Voice', isInVoice, true)
+            .addField(`Roles (${roleCount})`, roleNames.join(' '));
 
         await interaction.reply({ embeds: [embed] });
     },
