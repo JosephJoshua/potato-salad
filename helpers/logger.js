da056465edefb1fab5fd58f312111e9a6f830516
Sherrin0825 <24986026+Sherrin0825@users.noreply.github.com>
2022-05-29 09:48:01 +0000

    Fix stack trace in logger

--- helpers/logger.js
+++ helpers/logger.js
@@ -6,24 +6,24 @@ const formatISODate = () => {
 
 module.exports = {
     log: message => {
-        console.log(`[${formatISODate()}] LOG    ${message}`);
+        console.log(`[${formatISODate()}] LOG    `, message);
     },
     logError: message => {
-        console.log(`[${formatISODate()}] ${redBright('ERROR')}	 ${message}`);
+        console.log(`[${formatISODate()}] ${redBright('ERROR')}	 `, message);
     },
     logReady: message => {
-        console.log(`[${formatISODate()}] ${greenBright('READY')}	 ${message}`);
+        console.log(`[${formatISODate()}] ${greenBright('READY')}	 `, message);
     },
     logWarn: message => {
-        console.log(`[${formatISODate()}] ${yellowBright('WARN')}	 ${message}`);
+        console.log(`[${formatISODate()}] ${yellowBright('WARN')}	 `, message);
     },
     logLoad: message => {
-        console.log(`[${formatISODate()}] ${blueBright('LOAD')}	 ${message}`);
+        console.log(`[${formatISODate()}] ${blueBright('LOAD')}	 `, message);
     },
     logCache: message => {
-        console.log(`[${formatISODate()}] ${magentaBright('CACHE')}	 ${message}`);
+        console.log(`[${formatISODate()}] ${magentaBright('CACHE')}	 `, message);
     },
     logInit: message => {
-        console.log(`[${formatISODate()}] ${cyanBright('INIT')}	 ${message}`);
+        console.log(`[${formatISODate()}] ${cyanBright('INIT')}	 `, message);
     },
 };
