58f9d0078b7d8607f822436116352e06b4349880
Sherrin0825 <24986026+Sherrin0825@users.noreply.github.com>
2022-06-01 15:08:46 +0000

    Fix reloadCommands

--- reloadCommands.js
+++ reloadCommands.js
@@ -9,17 +9,17 @@ const dotenv = require('dotenv');
 dotenv.config();
 const { CLIENT_ID, DISCORD_TOKEN, GUILD_ID } = process.env;
 
-logger.logInit('Loading commands');
-
-const commands = [];
-requireFiles('./commands', command => {
-    commands.push(command.data.toJSON());
-    return `/${command.data.name}`;
-});
-
 const rest = new REST({ version: '9' }).setToken(DISCORD_TOKEN);
 
 (async () => {
+    logger.logInit('Loading commands');
+
+    const commands = [];
+    await requireFiles('./commands', command => {
+        commands.push(command.data.toJSON());
+        return `/${command.data.name}`;
+    });
+
     try {
         logger.logInit(`Reloading ${commands.length} commands`);
 
